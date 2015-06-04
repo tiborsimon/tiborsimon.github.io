d11375dde532b81916c2f3bb4e7487cc06036a5e
Tibor Simon <tibor@tibor-simon.com>
2015-06-04 20:28:47 +0200

    TOC development

--- assets/js/plugins/toc.js
+++ assets/js/plugins/toc.js
@@ -47,7 +47,7 @@
 
     var level = get_level(headers[0]),
       this_level,
-      html = settings.title + " <"+settings.listType+">";
+      html = " <"+settings.listType+">";
     headers.on('click', function() {
       if (!settings.noBackToTopLinks) {
         window.location.hash = this.id;
